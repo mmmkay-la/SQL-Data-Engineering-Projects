3e92e3b3566e293d46e0021e908a907ccd91513c
michaela <info.michaela.lm@gmail.com>
2026-07-23 16:44:30 +0800

    build sql to run all queries

---
+++ 2_Data_Warehouse_Mart_Build/build_queries.sql
@@ -0,0 +1,11 @@
+-- To run this script : .read 2_Data_Warehouse_Mart_Build/build_queries.sql
+
+
+-- Create Data Warehouse & Tables
+.read 2_Data_Warehouse_Mart_Build/01_create_dw_tables.sql
+
+-- Load Tables
+.read 2_Data_Warehouse_Mart_Build/02_load_schema_dw.sql
+
+-- Create & Load Flat Mart
+.read 2_Data_Warehouse_Mart_Build/03_create_flat_mart.sql
