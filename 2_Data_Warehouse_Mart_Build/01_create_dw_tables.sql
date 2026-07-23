b7bbda50da9600b419b8c0c581616f3feb321862
michaela <info.michaela.lm@gmail.com>
2026-07-23 11:15:39 +0800

    Created query for DW and Tables creation

---
+++ 2_Data_Warehouse_Mart_Build/01_create_dw_tables.sql
@@ -0,0 +1,70 @@
+-- STEP 1: Create Data Warehouse  & Tables
+
+-- Create Database
+CREATE DATABASE IF NOT EXISTS jobs_analysis_dw;
+
+USE jobs_analysis_dw;
+
+-- Drop Tables If Existing
+
+DROP TABLE IF EXISTS skills_dim;
+DROP TABLE IF EXISTS job_posts_fact;
+DROP TABLE IF EXISTS company_dim;
+DROP TABLE IF EXISTS skills_job_dim;
+
+CREATE TABLE IF NOT EXISTS company_dim(
+    company_id INTEGER PRIMARY KEY NOT NULL,
+    company_name VARCHAR,
+    link VARCHAR,
+    link_google VARCHAR,
+    thumbnail VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS job_posts_fact(
+    job_id      INTEGER PRIMARY KEY NOT NULL,
+    company_id  INTEGER REFERENCES company_dim(company_id),
+    job_title_short VARCHAR,
+    job_title       VARCHAR,
+    job_location    VARCHAR,
+    job_via         VARCHAR,
+    job_schedule_type   VARCHAR,
+    job_work_from_home  BOOLEAN,
+    search_location VARCHAR,
+    job_posted_date TIMESTAMP,
+    job_no_degree_mention   BOOLEAN,
+    job_health_insurace BOOLEAN,
+    job_country     VARCHAR,
+    salary_rate     VARCHAR,
+    salary_year_avg DOUBLE,
+    salry_hour_avg DOUBLE
+);
+
+CREATE TABLE IF NOT EXISTS skills_dim(
+    skill_id INTEGER PRIMARY KEY NOT NULL,
+    skill_name VARCHAR,
+    skill_type VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS skills_job_dim(
+    job_id INTEGER, 
+    skill_id INTEGER,
+    PRIMARY KEY (job_id, skill_id),
+    FOREIGN KEY (job_id) REFERENCES job_posts_fact(job_id),
+    FOREIGN KEY (skill_id) REFERENCES skills_dim(skill_id)
+);
+
+/* SOURCE FILES
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_dim.csv', 
+FROM read_csv('https://storage.googleapis.com/sql_de/job_postings_fact.csv',
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', 
+FROM read_csv('https://storage.googleapis.com/sql_de/company_dim.csv', 
+*/
+
+SELECT * FROM job_posts_fact;
+SELECT * FROM company_dim;
+SELECT * FROM skills_job_dim;
+SELECT * FROM skills_dim; 
+
+SELECT '=== DATA WAREHOUSE & TABLES CREATED ===' AS output;
+
+-- .read 2_Data_Warehouse_Mart_Build/01_create_dw_tables.sql
